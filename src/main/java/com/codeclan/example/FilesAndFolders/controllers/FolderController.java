0a3dee0125a4d664db0ae9f58f643610612df930
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-11 20:00:51 +0000

    Adds Create and Update routes to FolderController, tested in Insomnia

--- src/main/java/com/codeclan/example/FilesAndFolders/controllers/FolderController.java
+++ src/main/java/com/codeclan/example/FilesAndFolders/controllers/FolderController.java
@@ -5,9 +5,7 @@ import com.codeclan.example.FilesAndFolders.repositories.FolderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,17 @@ public class FolderController {
     @GetMapping(value = "/folders/{id}")
     public ResponseEntity getFolder(@PathVariable Long id){
         return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/folders")
+    public ResponseEntity<Folder> createFolder(@RequestBody Folder folder){
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/folders/{id}")
+    public ResponseEntity<Folder> updateFolder(@RequestBody Folder folder, @PathVariable Long id){
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
     }
 }
